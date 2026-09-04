9919ef6e6e9df877af877af49cbb7604ef12a186
arttop19-dev <arttop19@gmail.com>
2026-09-04 20:32:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Вставь свои ключи Supabase ниже!
 const SUPABASE_URL = 'https://ffgycumfccwcywyammzj.supabase.co/rest/v1/';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmZ3ljdW1mY2N3Y3l3eWFtbXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MjE4NTUsImV4cCI6MjEwNDA5Nzg1NX0.4QecPesjUcQaGXg7yBGqa1_ONIwPNbQWmsue9Spdrwc';
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let db = null;
 
